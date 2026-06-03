74a37ca8aa878ea0fa1ad0701bdec6d3e5fa1b5a
laurentdupin <48155177+laurentdupin@users.noreply.github.com>
2026-06-04 01:42:31 +0200

    Milestone: initialize HTTP name atoms

--- upstream/chromium/standalone_renderer/src/live_core_initializer_stub.cc
+++ upstream/chromium/standalone_renderer/src/live_core_initializer_stub.cc
@@ -116,7 +116,8 @@ void CoreInitializer::Initialize() {
   const unsigned kCoreStaticStringsCount =
       kQualifiedNamesCount + event_target_names::kNamesCount +
       event_type_names::kNamesCount + fetch_initiator_type_names::kNamesCount +
-      html_tokenizer_names::kNamesCount + trusted_types_names::kNamesCount;
+      html_tokenizer_names::kNamesCount + http_names::kNamesCount +
+      trusted_types_names::kNamesCount;
 
   StringImpl::ReserveStaticStringsCapacityForSize(
       kCoreStaticStringsCount + StringImpl::AllStaticStrings().size());
@@ -132,6 +133,7 @@ void CoreInitializer::Initialize() {
 
   event_target_names::Init();
   event_type_names::Init();
+  http_names::Init();
   html_tokenizer_names::Init();
   trusted_types_names::Init();
 }
