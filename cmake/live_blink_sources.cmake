47361187f80c9164b4421a55cf1f99c00e8fc274
laurentdupin <48155177+laurentdupin@users.noreply.github.com>
2026-06-12 19:50:24 +0200

    Use real viz copy output geometry utility

--- cmake/live_blink_sources.cmake
+++ cmake/live_blink_sources.cmake
@@ -49,6 +49,7 @@ set(BLINK_STANDALONE_LIVE_SOURCES
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/trees/effect_node.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/trees/sticky_position_constraint.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/trees/transform_node.cc
+  ${BLINK_STANDALONE_CHROMIUM_ROOT}/components/viz/common/frame_sinks/copy_output_util.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/components/viz/common/resources/shared_image_format.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/components/viz/common/resources/shared_image_format_utils.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/components/viz/common/surfaces/region_capture_bounds.cc
