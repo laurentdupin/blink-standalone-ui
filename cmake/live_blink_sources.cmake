f9710d264a902ee47fae2cb882c245529bfcfd71
laurentdupin <48155177+laurentdupin@users.noreply.github.com>
2026-06-13 20:59:32 +0200

    Use real cc list container support

--- cmake/live_blink_sources.cmake
+++ cmake/live_blink_sources.cmake
@@ -70,6 +70,7 @@ set(BLINK_STANDALONE_LIVE_SOURCES
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/base/value_iterators.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/base/values.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/animation/animation_id_provider.cc
+  ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/base/list_container_helper.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/debug/debug_colors.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/debug/layer_tree_debug_state.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/input/hit_test_opaqueness.cc
