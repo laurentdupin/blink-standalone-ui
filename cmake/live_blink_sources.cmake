cb703bba412da65ca1181b5213d2cd397fe18db6
laurentdupin <48155177+laurentdupin@users.noreply.github.com>
2026-06-13 21:39:24 +0200

    Use real cc compositor commit data support

--- cmake/live_blink_sources.cmake
+++ cmake/live_blink_sources.cmake
@@ -85,6 +85,7 @@ set(BLINK_STANDALONE_LIVE_SOURCES
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/layers/recording_source.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/layers/touch_action_region.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/raster/raster_source.cc
+  ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/trees/compositor_commit_data.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/trees/clip_node.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/trees/effect_node.cc
   ${BLINK_STANDALONE_CHROMIUM_ROOT}/cc/trees/occlusion.cc
